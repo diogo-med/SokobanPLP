9843822e3165f72afd78234507965945cbc9a260
diogo-med <diogo.medeiros@ccc.ufcg.edu.br>
2025-07-23 16:39:57 -0300

    update inicial

--- inicial.hs
+++ inicial.hs
@@ -89,69 +89,12 @@ gameLoopWithMap currentPlayerPos currentMap = do
                 else gameLoopWithMap newPos newMap
 
 -- Nova função para iniciar com caixas
-startGameWithBoxes :: IO ()
-startGameWithBoxes = do
+start :: IO ()
+start = do
     -- Cria mapa inicial com algumas caixas
     let initialMap = sokobanMap // [((1,1), Box), ((3,3), Box), ((2,1), Wall)]
     gameLoopWithMap (2, 2) initialMap
 
 
-
-
-
-
-
-
--- move :: Char -> (Int, Int) -> Array (Int, Int) Tile -> Bool -> (Int, Int)
--- move tecla (y, x) sokobanMap isPlayer = 
---     let newPos = calculateNewPos tecla (y, x)
---     in if inBounds (bounds sokobanMap) newPos then 
---         let tileAtNewPos = sokobanMap ! newPos   
---         in case tileAtNewPos of
---             Wall -> (y, x)  -- Parede: não move
---             Box -> 
---                 if isPlayer then
---                     -- Só jogador pode empurrar caixas
---                     let boxNewPos = move tecla newPos sokobanMap False  -- Caixa (False)
---                     in if boxNewPos /= newPos  -- Se caixa conseguiu mover
---                        then newPos             -- Jogador move
---                        else (y, x)             -- Caixa não moveu, jogador não move
---                 else 
---                     (y, x)  -- ❌ CONDIÇÃO DE PARADA: Caixa não empurra caixa
---             _ -> newPos  -- Floor/Mark: move normalmente
---        else (y, x)  -- Fora dos limites
-
--- -- Função wrapper para o jogador
--- movePlayer :: Char -> (Int, Int) -> Array (Int, Int) Tile -> (Int, Int)
--- movePlayer tecla pos sokobanMap = move tecla pos sokobanMap True
-
-
-
--- -- Atualizar gameLoop para usar a nova função
--- gameLoop :: (Int, Int) -> IO ()
--- gameLoop currentPlayerPos = do
---     putStrLn "=== SOKOBAN ==="
---     printMap sokobanMap currentPlayerPos
---     putStrLn $ "Posição: " ++ show currentPlayerPos
---     putStrLn "Use w/a/s/d para mover, q para sair"
-    
---     tecla <- getChar
---     putStrLn ""
-    
---     if tecla == 'q'
---         then putStrLn "Fim do jogo!"
---         else do
---             let newPos = movePlayer tecla currentPlayerPos sokobanMap
---             if newPos == currentPlayerPos && tecla `elem` "wasd"
---                 then do
---                     putStrLn "Movimento inválido!"
---                     gameLoop currentPlayerPos
---                 else gameLoop newPos
-
--- startGame :: IO ()
--- startGame = gameLoop (2, 2)
-
--- Sintaxe: array // [(posição, novoValor), ...]
--- sokobanMap // [(newPos, Floor), (boxNewPos, Box)]
 
 
